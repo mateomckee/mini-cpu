423a218b5c9d590788f5a8178263de680c5c3800
no0byfibby <akunnechinedum@gmail.com>
2026-04-09 19:32:38 -0500

    Completed Milestone #2. Please review that it's correct, and all specifics of the changes are in the README file.

--- README.txt
+++ README.txt
@@ -1,1 +1,19 @@
 mini-cpu
+
+Milestone #2 is implemented at a basic level in `src/sim.c`.
+
+Build:
+`make`
+
+Run:
+`./sim.o -s <cache_kb> -b <block_bytes> -a <assoc> -r <rr|rnd> -p <phys_mem_mb> -u <os_percent> -n <timeslice|-1> -f <trace1> [-f <trace2>] [-f <trace3>]`
+
+Current scope:
+- Keeps Milestone #1 header/calculation output.
+- Adds virtual memory simulation for 1 to 3 trace files.
+- Uses 4 KB pages and one page table per process.
+- Supports free-page allocation and replacement on page faults.
+- Prints Milestone #2 virtual memory stats and per-process page table usage.
+
+Not included:
+- Milestone #3 cache simulation.
--- src/sim.h
+++ src/sim.h
@@ -1,15 +1,19 @@
+#ifndef SIM_H
+#define SIM_H
+
 #define MAX_TRC_FILES 3
-#define MIN_ARGS 17
+#define PAGE_SIZE 4096U
+#define PAGE_TABLE_ENTRIES 524288U
 
 typedef struct {
     int cache_size;
     int block_size;
     int associativity;
-    char* replacement_policy;
+    char *replacement_policy;
     int physical_memory;
     float physical_memory_usage_percentage;
-    float instructions_per_timeslice;
-    char* trace_files[MAX_TRC_FILES];
+    int instructions_per_timeslice;
+    char *trace_files[MAX_TRC_FILES];
     int num_trace_files;
 } SimConfig;
 
@@ -18,14 +22,43 @@ typedef struct {
     int tag_bits;
     int total_rows;
     int total_blocks;
-    int overhead_bytes; 
-    int implementation_bytes; 
+    int overhead_bytes;
+    int implementation_bytes;
     float cost;
 } CacheCalc;
 
 typedef struct {
     long num_physical_pages;
     long num_system_pages;
+    long num_user_pages;
     int pte_bits;
     int page_table_bytes;
+    int per_process_page_table_bytes;
 } PhysicalCalc;
+
+typedef struct {
+    int valid;
+    unsigned int physical_page;
+} PageTableEntry;
+
+typedef struct {
+    FILE *file;
+    char *name;
+    int done;
+    long used_entries;
+    PageTableEntry *page_table;
+} TraceProcess;
+
+typedef struct {
+    int owner_process;
+    unsigned int owner_virtual_page;
+} PhysicalPageFrame;
+
+typedef struct {
+    long virtual_pages_mapped;
+    long page_table_hits;
+    long pages_from_free;
+    long total_page_faults;
+} VmStats;
+
+#endif
